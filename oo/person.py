e834f643de845a17e1ecc079ede1851371c38867
djonypaladin <dhomynyfarias3@gmail.com>
2020-06-08 12:57:25 -0300

    my first lines of code

--- oo/person.py
+++ oo/person.py
@@ -1,8 +1,14 @@
 class Pessoa:
       def comprimentar(self):
           return 'hello my code'
+      def despedisir(self):
+          return 'tchau ;3'
+      def contar_1(self):
+          return list(range(10,0,-1))
 
 if __name__ == '__main__':
     p = Pessoa()
     type (p)
     print(p.comprimentar())
+    print(p.contar_1())
+    print(p.despedisir())
