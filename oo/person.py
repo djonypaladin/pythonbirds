9ac0754145d0a6cc2863c2291bd2f75bae10228a
djonypaladin <dhomynyfarias3@gmail.com>
2020-06-10 09:53:16 -0300

    my first lines of code

--- oo/person.py
+++ oo/person.py
@@ -1,15 +1,26 @@
 class Pessoa:
-      def __init__(self,*familia,name=None,idade=17):
+    menbros_superiores = 2
+    menbro_inferiores=2
+    def __init__(self,*familia,name=None,idade=17):
          self.name= name
          self.familia= list(familia)
          self.idade= idade
 
-      def comprimentar(self):
+    def comprimentar(self):
           return 'hello my code'
-      def despedisir(self):
+    def despedisir(self):
           return 'diz tchau ;3'
-      def contar_1(self):
-          return list(range(10,0,-1))
+    def contar_1(self):
+     return list(range(10,1,-1))
+
+    @staticmethod
+    def estatico ():
+     return 50
+
+    @classmethod
+    def atributodeclasse(cls):
+        return f'{cls} - membros superiores {cls.menbros_superiores},membros inferiores {cls.menbro_inferiores}'
+
 
 if __name__ == '__main__':
     djony = Pessoa(name='djony', idade=17)
@@ -22,3 +33,5 @@ if __name__ == '__main__':
     for familia in mother.familia:
      print(familia.name,familia.idade)
      print(djony.__dict__)
+     print(Pessoa.estatico())
+     print(Pessoa.atributodeclasse())
